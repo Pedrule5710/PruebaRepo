e74a4f797a85cdbe7a18b55364c60a587d52baad
Pedrule5710 <m.r.pedrule@gmail.com>
2026-02-27 12:20:31 +0100

    feat: Añadimos var1 para imprimir un mensaje real 100%

--- src/solucion/Principal.java
+++ src/solucion/Principal.java
@@ -5,6 +5,8 @@ public class Principal {
 	
 	public static void main(String args[]) {
 		System.out.println("Hola mundo");
+		String var1 = "Erik no sabe hacer la O con un canuto";
+		System.out.println(var1);
 		
 	}
 
